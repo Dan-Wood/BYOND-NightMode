146c54b70b59e8569243e75b23c28cea60236983
Dan Wood <a.t.h.k@hotmail.com>
2015-02-17 18:47:09 +1100

    Toggle time!

--- Extensions/Chrome/js/custom.js
+++ Extensions/Chrome/js/custom.js
@@ -1,10 +1,14 @@
 jQuery('body').append('<div id="nightmode"><span class="eye"></span></div>');
 
+var link = '<link id="nightmodecss" rel="stylesheet" type="text/css" href="https://byondpanel.com/BYOND-NightMode/nightmode.css">';
+
+if(localStorage.nightmode == 1) {
+    jQuery('body').append(link);
+}
 
 jQuery('#nightmode').click(function() {
     console.log(localStorage.nightmode);
-    if(!localStorage.nightmode || localStorage.nightmode == 0) {
-        var link = '<link id="nightmodecss" rel="stylesheet" type="text/css" href="https://byondpanel.com/BYOND-NightMode/nightmode.css">';
+    if(localStorage.nightmode == 0) {
         jQuery('body').append(link);
         localStorage.nightmode = 1;
     } else {
